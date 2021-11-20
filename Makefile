f92c4cd72ced9a66422f8f449ade30771455c50d
solrak <luiscarlosquesada@gmail.com>
2021-11-19 19:12:41 -0600

    Soporte para c++

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 INCLUDE = Src/
 
 lucoa: syntactical.tab.c syntactical.tab.h lex.yy.c
-	gcc lex.yy.c syntactical.tab.c -I $(INCLUDE) -o lucoa 
+	g++ lex.yy.c syntactical.tab.c -I $(INCLUDE) -o lucoa 
 	rm syntactical.tab.c syntactical.tab.h lex.yy.c
 
 lex.yy.c: syntactical.tab.c syntactical.tab.h Src/lexical.lex
 	lex Src/lexical.lex 
 
 syntactical.tab.c syntactical.tab.h: Src/syntactical.y
-	bison -d Src/syntactical.y  
+	bison -d Src/syntactical.y 
--- Src/SymbolTable.h
+++ Src/SymbolTable.h
@@ -3,14 +3,58 @@
 
 #include <stdio.h>
 
+//Table implementation on a linked list coz bruh,
+//im not doing a hashtable in C... im rewriting
+//everything in rust eventually
+
 struct SymbolEntry
 {
-    char* name;
-    char* type;
+    int name;
+    int type;
 };
 
-void tableInsertion(int a, int b){
-    printf("Implementar la tabla\n");
+struct Node
+{
+    struct SymbolEntry data;
+    struct Node *next;
+};
+
+struct Node *table = NULL;
+
+void listInsert(struct Node *node)
+{
+    if (table == NULL)
+    {
+        table = node;
+    }
+    else
+    {
+        struct Node *t = table;
+        while (t->next)
+        {
+            t = t->next;
+        }
+        t->next = node;
+    }
+}
+
+void printTable()
+{
+    struct Node *t = table;
+    while (t)
+    {
+        printf("Name: %d    Type: %d", t->data.name, t->data.type);
+        t = t->next;
+    }
+}
+
+void tableInsertion(int a, int b)
+{
+    struct Node * node = new Node()
+    {
+        /* data */
+    };
+    
 }
 
 #endif
